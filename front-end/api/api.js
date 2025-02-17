c4602c6858f294b0d37b814f4f1c22a3621fdba9
Luan Alves <luanWeb2024tab@gmail.com>
2025-02-16 22:49:20 -0300

    Projeto Finalizado com ajuste no Deploy

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,8 @@
 import "dotenv/config";
 import axios from "axios";
 
-const NODE_ENV = process.env.NODE_ENV;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+//const NODE_ENV = process.env.NODE_ENV;
+const URL = "http://localhost:3001/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
